3fc24d50c040ded884706a84da8c2a95d46bd335
Marco Dugatto <dughi.boogie@gmail.com>
2024-07-24 20:30:17 +0200

    Full working puzzle

--- Scripts/PuzzleSystem/Puzzle.gd
+++ Scripts/PuzzleSystem/Puzzle.gd
@@ -10,18 +10,32 @@ var current_puzzle_items: Array
 @export var order_needed: bool
 
 @export var reward: InventoryItemData
+var puzzle_solved := false
 
 
 func add_puzzle_item(item: InventoryItemData):
+	if puzzle_solved:
+		return
+	
 	current_puzzle_items.append(item)
 	
 	print("Added puzzle item " + item.item_name)
 	
-	if current_puzzle_items.all(func(puzzle_item): return puzzle_solution_items.has(puzzle_item)):
+	# Check if all solution items are present
+	var missing_puzzle_item = false
+	for puzzle_item in puzzle_solution_items:
+		if !current_puzzle_items.has(puzzle_item):
+			missing_puzzle_item = true
+	if not missing_puzzle_item:
 		print("Puzzle solved!")
+		InventorySystem.add_item(reward)
+		puzzle_solved = true
 
 
 func remove_puzzle_item(item: InventoryItemData):
+	if puzzle_solved:
+		return
+	
 	print("Removed puzzle item " + item.item_name)
 	
 	current_puzzle_items.erase(item)
