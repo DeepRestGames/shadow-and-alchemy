04395cd69105cb5b9d864eb391bfea2b908874e1
Gamma_Sigma <lateo1234@gmail.com>
2024-07-25 21:56:28 +0200

    Normalise sound + remove bad sample
    - The volume of all footsteps has been normalised to avoid extreme dynamic variability.
    - A footstep sample with a lot of background noise has been removed from the pool.
    - `MainScene_Prototype01.tscn` had to be edited manually to make up for the removed file.
    - `MainScene_Prototype01.gd`: added more footstep sounds + minor refactoring.

--- Scripts/MainScene_Prototype01.gd
+++ Scripts/MainScene_Prototype01.gd
@@ -1,29 +1,40 @@
 extends Node3D
 
-var footstep_sounds = [preload("res://Assets/Audio/Sound/footsteps_wood_single_1.mp3"), preload("res://Assets/Audio/Sound/footsteps_wood_single_2.mp3"), preload("res://Assets/Audio/Sound/footsteps_wood_single_3.mp3")]
+# Preload sounds to randomise them later
+const footstep_sounds = [
+	preload("res://Assets/Audio/Sound/footsteps_wood_single_2.mp3"),
+	preload("res://Assets/Audio/Sound/footsteps_wood_single_3.mp3"),
+	preload("res://Assets/Audio/Sound/footsteps_wood_single_4.mp3"),
+	preload("res://Assets/Audio/Sound/footsteps_cellar_single_1.mp3"),
+	preload("res://Assets/Audio/Sound/footsteps_cellar_single_2.mp3"),
+	preload("res://Assets/Audio/Sound/footsteps_cellar_single_3.mp3"),
+	preload("res://Assets/Audio/Sound/footsteps_cellar_single_4.mp3"),
+	preload("res://Assets/Audio/Sound/footsteps_cellar_single_5.mp3"),
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Prepare creepy background music stream
+	# Prepare audio
 	var audio_stream = $BackgroundMusic.stream as AudioStreamMP3
+	var player = get_node("Player")
+
 	audio_stream.loop = true
 	audio_stream.loop_offset = 21.372
 
-	# Start creepy background music stream when player emits `startBackgroundMusic` signal
-	var player = get_node("Player")
 	player.connect("startBackgroundMusic", play_backgroundMusic)
-
-	# Load multiple footsteps to be called randomly when player walks
 	player.connect("playerMoved", play_footstep)
+
 
 func play_backgroundMusic():
 	if not $BackgroundMusic.is_playing():
 		$BackgroundMusic.play()
 
+
 func play_footstep():
 	var random_footstep = footstep_sounds[randi() % footstep_sounds.size()]
 	$Footsteps.stream = random_footstep
 	$Footsteps.play()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
