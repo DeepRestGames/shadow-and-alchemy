63840e1e7a58fad4a176503aed5b4c3db6853923
Marco Dugatto <dughi.boogie@gmail.com>
2024-07-27 00:20:28 +0200

    Alchemical circle
    - UI for selecting symbols
    - Alchemical circle scene
    - Alchemical circle sprite

--- Scripts/PuzzleSystem/AlchemicalProcessSymbol.gd
+++ Scripts/PuzzleSystem/AlchemicalProcessSymbol.gd
@@ -2,9 +2,34 @@ class_name AlchemicalProcessSymbol
 extends Prop
 
 
+enum AlchemicalProcesses {
+	CALCINATION,
+	CONGELATION,
+	FIXATION,
+	SOLUTION,
+	DIGESTION,
+	DISTILLATION,
+	SUBLIMATION,
+	SEPARATION,
+	CERATION,
+	FERMENTATION,
+	MULTIPLICATION,
+	PROJECTION
+}
+
+
 @onready var process_icon = $ProcessIcon
 @onready var alchemical_symbols_choice = $AlchemicalSymbolsChoice
 
 
+func _ready():
+	alchemical_symbols_choice.hide()
+
+
 func _interacted():
-	print("Alchemical process interacted!")
+	alchemical_symbols_choice.show()
+
+
+func new_process_chosen(icon: Texture2D):
+	process_icon.texture = icon
+	alchemical_symbols_choice.hide()
