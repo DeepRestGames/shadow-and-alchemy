0c14e3aea9605cefc5b188927a08529ef68ecf2a
Marco Dugatto <dughi.boogie@gmail.com>
2024-07-27 16:49:49 +0200

    Reusable puzzle items

--- Scripts/PuzzleSystem/PuzzleSlot.gd
+++ Scripts/PuzzleSystem/PuzzleSlot.gd
@@ -2,23 +2,27 @@ class_name PuzzleSlot
 extends Node3D
 
 
-#@export var related_puzzles: Array[Puzzle]
-
 @onready var alchemical_circle = $".."
 
 @onready var item_spawn_origin = $ItemSpawnOrigin
 var current_dropped_item: InventoryItemData
 var current_dropped_item_scene = null
+var current_prop_pickup_scene: Prop_Pickup = null
 
 
 func item_dropped(item: InventoryItemData):
-	# Swap dropped item with already present one
+	# Swap dropped item with already present one	
 	if current_dropped_item != null:
-		InventorySystem.add_item(current_dropped_item)
-		current_dropped_item_scene.queue_free()
-		
-		item_removed(current_dropped_item)
-	
+		var pickup_scene_to_remove = null
+		if current_dropped_item_scene is Prop_Pickup:
+			pickup_scene_to_remove._interacted()
+		else:
+			for node in current_dropped_item_scene.get_children(true):
+				if node is Prop_Pickup:
+					pickup_scene_to_remove = node as Prop_Pickup
+					pickup_scene_to_remove._interacted()
+					break
+
 	print("Item " + item.item_name + " dropped in slot!")
 	current_dropped_item = item
 	
@@ -29,25 +33,24 @@ func item_dropped(item: InventoryItemData):
 	item_scene_instance.global_position = item_spawn_origin.global_position
 	
 	# Get node in model scene with Prop_Pickup script
-	var pickup_item_scene = null
 	if item_scene_instance is Prop_Pickup:
-		pickup_item_scene = item_scene_instance
+		current_prop_pickup_scene = item_scene_instance
 	else:
 		for node in item_scene_instance.get_children(true):
 			if node is Prop_Pickup:
-				pickup_item_scene = node as Prop_Pickup
+				current_prop_pickup_scene = node as Prop_Pickup
 				break
-	item_scene_instance.global_position.y += pickup_item_scene.item_model_height / 2
+	item_scene_instance.global_position.y += current_prop_pickup_scene.item_model_height / 2
 	current_dropped_item_scene = item_scene_instance
+	current_prop_pickup_scene.dropped_in_puzzle_slot = self
 	
 	# Add item to puzzles related to slot
 	for puzzle in alchemical_circle.related_puzzles:
 		puzzle.add_puzzle_item(item)
-
-
-func _process(_delta):
-	if current_dropped_item != null and current_dropped_item_scene.is_queued_for_deletion():
-		item_removed(current_dropped_item)
+	
+	# Remove from inventory if item is not reusable
+	if not current_dropped_item.is_reusable:
+		InventorySystem.remove_item(current_dropped_item)
 
 
 func item_removed(item: InventoryItemData):
@@ -59,3 +62,4 @@ func item_removed(item: InventoryItemData):
 	
 	current_dropped_item = null
 	current_dropped_item_scene = null
+	current_prop_pickup_scene = null
