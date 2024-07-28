abb54c8b4c12fbbdbbb0e0faacbb373545f0c0ff
enirfo <enrico.fornasa.it@gmail.com>
2024-07-28 18:48:50 +0200

    books + some props in main scene

---
+++ Scripts/Items/Book.gd
@@ -0,0 +1,17 @@
+extends CSGBox3D
+
+@onready var item_book_black = $"."
+@onready var sprite_3d = $Lable/Sprite3D
+@onready var sprite_3d_2 = $Sprite3D2
+@export var texture: Texture2D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	sprite_3d.texture=texture
+	sprite_3d_2.texture=texture
+	pass # Replace with function body.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+	
+
