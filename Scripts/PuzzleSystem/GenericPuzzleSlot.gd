d1a15c781d00e43fc67763d5a0063c9aa413d279
Marco Dugatto <dughi.boogie@gmail.com>
2024-07-28 23:55:39 +0200

    Merge branch development into wip/puzzles

--- Scripts/PuzzleSystem/GenericPuzzleSlot.gd
+++ Scripts/PuzzleSystem/GenericPuzzleSlot.gd
@@ -4,19 +4,26 @@ extends PuzzleSlot
 signal item_was_dropped
 
 @export var related_puzzles: Array[Puzzle]
+var currently_dropped_items: Array[InventoryItemData]
+
+@export var model_variation: Node3D
 
 
 func item_dropped(item: InventoryItemData):
 	item_was_dropped.emit()
 	print("GENERIC PUZZLE item_was_dropped") # TODO: debug print
 	print("Item " + item.item_name + " dropped in slot!")
-
-	if not item.is_reusable:
-		print("Can't put this object here!")
-
+	
+	currently_dropped_items.append(item)
+	
 	# Add item to puzzles related to slot
 	for puzzle in related_puzzles:
 		puzzle.add_puzzle_item(item)
+	
+	InventorySystem.remove_item(item)
+	
+	if model_variation != null:
+		model_variation.show()
 
 
 func item_removed(_item: InventoryItemData):
@@ -25,6 +32,13 @@ func item_removed(_item: InventoryItemData):
 
 func remove_items():
 	print("Removed items from puzzle!")
-
+	
+	for item in currently_dropped_items:
+		InventorySystem.add_item(item)
+	currently_dropped_items.clear()
+	
 	for puzzle in related_puzzles:
 		puzzle.clear_all()
+	
+	if model_variation != null:
+		model_variation.hide()
