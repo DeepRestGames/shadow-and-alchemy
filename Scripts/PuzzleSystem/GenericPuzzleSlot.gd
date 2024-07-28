8b8f3f585d37aa0705a472abe173936f8d01dd94
Marco Dugatto <dughi.boogie@gmail.com>
2024-07-29 00:27:08 +0200

    Super ugly logic to make crucible visible in fireplace

--- Scripts/PuzzleSystem/GenericPuzzleSlot.gd
+++ Scripts/PuzzleSystem/GenericPuzzleSlot.gd
@@ -8,11 +8,43 @@ var currently_dropped_items: Array[InventoryItemData]
 
 @export var model_variation: Node3D
 
+# Super ugly logic just for crucible
+var crucible = null
+var crucible_coin = null
+var crucible_melt = null
+
+
+func _ready():
+	# Super ugly logic just for crucible
+	if get_parent() != null and get_parent().name == "Fireplace":
+		crucible = $"../Crucible"
+		crucible_coin = $"../Crucible/CrucibleCoin"
+		crucible_melt = $"../Crucible/MoltenSilver"
+
 
 func item_dropped(item: InventoryItemData):
 	item_was_dropped.emit()
 	print("GENERIC PUZZLE item_was_dropped") # TODO: debug print
 	print("Item " + item.item_name + " dropped in slot!")
+	
+	
+	# Super ugly logic just for crucible
+	if crucible != null:
+		if currently_dropped_items.is_empty():
+			if item.item_name == "Crucible":
+				crucible.show()
+			else:
+				return
+		
+		else:
+			if item.item_name == "Silver Coin":
+				crucible_coin.show()
+				await get_tree().create_timer(1.5).timeout
+				crucible_coin.hide()
+				crucible_melt.show()
+				await get_tree().create_timer(1.5).timeout
+				crucible.hide()
+	
 	
 	currently_dropped_items.append(item)
 	
