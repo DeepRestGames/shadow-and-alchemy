3edbc9b51efe6dd40960d15b41c8bf666826b3bd
Gamma_Sigma <lateo1234@gmail.com>
2024-07-29 21:25:54 +0200

    Create `Audio` scene
    - `Audio` scene has been created to relieve the Main Scene from the responsibility to manage sounds.
    - Audio management code has migrated to `Audio.gd`. `MainScene.gd` is now almost empty.
    - `GenericPuzzleSlot.gd` modified to also emit a generic `item_was_interacted` signal.

--- Scripts/PuzzleSystem/GenericPuzzleSlot.gd
+++ Scripts/PuzzleSystem/GenericPuzzleSlot.gd
@@ -1,7 +1,7 @@
 class_name GenericPuzzleSlot
 extends PuzzleSlot
 
-signal item_was_dropped
+signal item_was_interacted
 
 @export var related_puzzles: Array[Puzzle]
 var currently_dropped_items: Array[InventoryItemData]
@@ -23,11 +23,11 @@ func _ready():
 
 
 func item_dropped(item: InventoryItemData):
-	item_was_dropped.emit()
-	print("GENERIC PUZZLE item_was_dropped") # TODO: debug print
+	item_was_interacted.emit()
+	print("GENERIC PUZZLE item_was_interacted") # TODO: debug print
 	print("Item " + item.item_name + " dropped in slot!")
-	
-	
+
+
 	# Super ugly logic just for crucible
 	if crucible != null:
 		if currently_dropped_items.is_empty():
@@ -35,7 +35,7 @@ func item_dropped(item: InventoryItemData):
 				crucible.show()
 			else:
 				return
-		
+
 		else:
 			if item.item_name == "Silver Coin":
 				crucible_coin.show()
@@ -44,16 +44,16 @@ func item_dropped(item: InventoryItemData):
 				crucible_melt.show()
 				await get_tree().create_timer(1.5).timeout
 				crucible.hide()
-	
-	
+
+
 	currently_dropped_items.append(item)
-	
+
 	# Add item to puzzles related to slot
 	for puzzle in related_puzzles:
 		puzzle.add_puzzle_item(item)
-	
+
 	InventorySystem.remove_item(item)
-	
+
 	if model_variation != null:
 		model_variation.show()
 
@@ -64,13 +64,13 @@ func item_removed(_item: InventoryItemData):
 
 func remove_items():
 	print("Removed items from puzzle!")
-	
+
 	for item in currently_dropped_items:
 		InventorySystem.add_item(item)
 	currently_dropped_items.clear()
-	
+
 	for puzzle in related_puzzles:
 		puzzle.clear_all()
-	
+
 	if model_variation != null:
 		model_variation.hide()
